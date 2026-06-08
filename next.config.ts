c82607d91835c802238848c4690473db856e5655
Ngan Nguyen <saoaz1029@gmail.com>
2026-06-08 19:02:31 +0700

    feat: setup proxy to bypass mixed content for HTTP backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // Proxy tất cả request bắt đầu bằng /proxy/api sang Backend HTTP
+        source: "/proxy/api/:path*",
+        destination: "http://mirai.runasp.net/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
